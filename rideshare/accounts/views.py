89d51ce49f0e1f255783062531717ec30138f0c8
himanshu <ho2es@virginia.edu>
2016-02-10 14:26:26 -0500

    wsgi instead of normal

--- rideshare/accounts/views.py
+++ rideshare/accounts/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
 def home(request):
-    return render(request, 'index.html')
+    #return render(request, 'index.html')
+    now = datetime.datetime.now()
+    html = "<html><body>It is now sometime.</body></html>"
+    return HttpResponse(html)
--- rideshare/rideshare/settings.py
+++ rideshare/rideshare/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'mod_wsgi.server',
 )
 
 MIDDLEWARE_CLASSES = (
