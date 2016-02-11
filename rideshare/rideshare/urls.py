3ab58742572ca72a7b92033e6b7084b123a643aa
revanthkolli <revanthkolli@gmail.com>
2016-02-11 15:57:21 -0500

    fixed syntax error

--- rideshare/rideshare/urls.py
+++ rideshare/rideshare/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from accounts import views as accounts_views
 
 urlpatterns = [
     # Examples:
@@ -9,5 +8,5 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'accounts.views.home', name='home')
-    url(r'^accounts/', include('accounts.urls', namespace='accounts'), name='accounts'),
+    url(r'^accounts/', include(accounts.urls)),
 ]
