26900ebf7f01c4ec1136f16a4dd55c4194be8bda
revanthkolli <revanthkolli@gmail.com>
2016-02-14 18:34:35 -0500

    testing json response

--- rideshare/accounts/views.py
+++ rideshare/accounts/views.py
@@ -26,8 +26,8 @@ def user(request, id):
             data = {'message': 'user with id ' + id + ' was not found.', 'status': str(HTTP_404_NOT_FOUND)}
             return JsonResponse(data, status=HTTP_404_NOT_FOUND)
     else:
-        html = "Updating user"
-        return HttpResponse(html)
+        # data = json.loads(request.body)
+        return JsonResponse(request.body)
 
 @require_http_methods(["PUT"])
 def create_user(request):
