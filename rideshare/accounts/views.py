aed667337f9964bfc939e1d98ab022ce61ef4518
himanshu <ho2es@virginia.edu>
2016-02-14 19:48:25 -0500

    create user

--- rideshare/accounts/views.py
+++ rideshare/accounts/views.py
@@ -3,13 +3,13 @@ from django.http import HttpResponse, JsonResponse
 import json
 from django.views.decorators.http import require_http_methods
 from accounts.status_codes import *
-from django.views.decorators.csrf import csrf_protect
-
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+from django.contrib.auth.models import User
 
 from accounts.models import UserProfile
 
 def home(request):
-    html = "<html><head><title>Welcome to Rideshare</title></head><body><h1>Welcome to Rideshare!</h1></body></html>"
+    html = "<html><head><title>Welcome to Rideshare!</title></head><body><h1>Welcome to Rideshare!</h1></body></html>"
     return HttpResponse(html)
 
 # GET or UPDATE user
@@ -25,14 +25,17 @@ def user(request, id):
             data = {'status': str(HTTP_404_NOT_FOUND), 'message': 'user with ' + id + ' was not found.'}
             return JsonResponse(data, status=HTTP_404_NOT_FOUND)
 
+@csrf_exempt
 @require_http_methods(["PUT"])
-def create_user(request):
-    return JsonResponse({'ok', 'it worked'})
-    # new_user = User.objects.create_user(request.POST['username'], email=request.POST['email'], password=request.POST['password'], **request.POST)
-    # new_user_profile = UserProfile(user=user, phone=request.POST['phone'], school=request.POST['school'], rating=request.POST['rating'])
-    # new_user_profile.save()
-    # data = {'status': str(HTTP_201_CREATED),'id': str(new_user_profile.id), 'email': new_user_profile.email, 'first_name': new_user_profile.first_name, 'last_name': new_user_profile.last_name, 'number': new_user_profile.phone, 'school': new_user_profile.school, 'rating': str(new_user_profile.rating)}
-    # return JsonResponse(data, status=HTTP_201_CREATED)
+def create_user(request):    
+    data = json.loads(request.body.decode("utf-8"))
+    new_user = User.objects.create_user(username=data['email'], email=data['email'], password=data['password'], first_name=data['first_name'], last_name=data['last_name'])
+    new_user_profile = UserProfile(user=new_user, phone=data['phone'], school=data['school'], rating=0)
+
+    new_user_profile.save()
+    dataresult = {'status': str(HTTP_201_CREATED),'id': str(new_user_profile.id), 'email': new_user_profile.user.email, 'first_name': new_user_profile.user.first_name, 'last_name': new_user_profile.user.last_name, 'phone': new_user_profile.phone, 'school': new_user_profile.school, 'rating': str(new_user_profile.rating)}
+    return JsonResponse(dataresult, status=HTTP_201_CREATED)
+
 
 
 # SERVICES  list
