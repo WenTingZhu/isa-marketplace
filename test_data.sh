68c3c4f36a4293876ec10cca2c175f92bd44b410
himanshu <himanshuo@gmail.com>
2016-03-22 02:18:58 -0400

    tagable

--- test_data.sh
+++
@@ -1,2 +0,0 @@
-curl -H "Content-Type: application/json" -X PUT -d '{"email":"johndoe@email.com","password":"password", "first_name": "John", "last_name": "Doe", "phone": "7777777777", "school":"UVA"}' http://localhost:8000/api/v1/accounts/user/
-curl -H "Content-Type: application/json" -X PUT -d '{"driver":"1","password":"password", "first_name": "John", "last_name": "Doe", "phone": "7777777777", "school":"UVA"}' http://localhost:8000/api/v1/accounts/user/
