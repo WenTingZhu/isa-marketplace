481f2d1391c07c170dba7366f98f220c7f87f0e2
Wen-Ting Zhu <wz6hd@virginia.edu>
2016-04-18 15:20:30 -0400

    init selenium

---
+++ frontend/frontend/tests.py
@@ -0,0 +1,30 @@
+import os, unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# Before building ensure that you have the most recent chromedriver available on your $PATH.
+# https://sites.google.com/a/chromium.org/chromedriver/downloads
+chromedriver = "/home/wzhu/Downloads/chromedriver"
+os.environ["webdriver.chrome.driver"] = chromedriver
+# # driver = webdriver.Chrome(chromedriver)
+# # driver.get('http://seleniumhq.org/')
+#
+# browser = webdriver.Chrome(chromedriver)
+# browser.get('http://www.yahoo.com')
+# assert 'Yahoo' in browser.title
+#
+# elem = browser.find_element_by_name('p')  # Find the search box
+# elem.send_keys('seleniumhq' + Keys.RETURN)
+
+class GoogleTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome(chromedriver)
+        self.addCleanup(self.browser.quit)
+
+    def testPageTitle(self):
+        self.browser.get('http://localhost:8002/')
+        self.assertIn('Google', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
